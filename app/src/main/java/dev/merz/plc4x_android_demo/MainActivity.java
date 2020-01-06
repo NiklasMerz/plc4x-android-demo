3158db563e4934b96b97d53ba166c243e21efcc2
Niklas Merz <niklasmerz@apache.org>
2020-01-07 00:10:01 +0100

    Get Log4J running

--- app/src/main/java/dev/merz/plc4x_android_demo/MainActivity.java
+++ app/src/main/java/dev/merz/plc4x_android_demo/MainActivity.java
@@ -1,8 +1,14 @@
 package dev.merz.plc4x_android_demo;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -10,14 +16,35 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.apache.log4j.Level;
+import org.apache.log4j.chainsaw.Main;
 import org.apache.plc4x.java.PlcDriverManager;
 import org.apache.plc4x.java.api.PlcConnection;
+
+import de.mindpipe.android.logging.log4j.LogConfigurator;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Log.d("PLC4X", "No permission");
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
+        final LogConfigurator logConfigurator = new LogConfigurator();
+
+        logConfigurator.setFileName(getApplicationContext().getExternalFilesDir("log").getAbsolutePath() + "app.log");
+        logConfigurator.setRootLevel(Level.DEBUG);
+        // Set log level of a specific logger
+        logConfigurator.setLevel("org.apache", Level.ERROR);
+        logConfigurator.configure();
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -28,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                   //      .setAction("Action", null).show();
+
 
                 String connectionString = "s7://10.10.64.20/1/1";
 
